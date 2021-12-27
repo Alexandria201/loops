27caa5d22b317adbd01b9948553c62b2e1c1a184
Alexandria201 <aov6lt@ukr.net>
2021-12-27 13:51:30 +0200

    used loops for, do/while,while

---
+++ loops.js
@@ -0,0 +1,16 @@
+for (let forCount = 1; forCount <= 8; forCount++) {
+    console.log(forCount);
+}
+
+let doCount = 1;
+do {
+    console.log(doCount)
+    doCount++
+} while (doCount <= 8)
+
+
+let whileCount = 1;
+while (whileCount <= 8) {
+    console.log(whileCount);
+    whileCount++;
+}
